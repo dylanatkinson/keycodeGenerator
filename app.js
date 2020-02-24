be540f67cbff7650b300f909ecec56b3533f8c8f
dylanatkinson <dylanjamesatkinson@gmail.com>
2020-02-24 15:23:23 +0000

    array to show previously used keys added

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const keyCode = document.getElementById("keyedIn");
 const keyEventPlaceholder = document.getElementById("keyEventPlaceholder");
 const whichEventPlaceholder = document.getElementById("whichEventPlaceholder");
 const codeEventPlaceholder = document.getElementById("codeEventPlaceholder");
+const keyedPlaceholder = document.getElementById("keyedPlaceholder");
 
-keyCode.addEventListener("keydown", () => {
+let keyedHistory = [];
+
+keyCode.addEventListener("keypress", () => {
+    // resets the input box once the key has been pressed, showing only one key at a time
     keyCode.value = "";
+
+    // assigns keypresses to variables then displays them separately based on actual value, key code, and key name 
     eventKey = event.key;
     keyEventPlaceholder.textContent = eventKey;
     whichCode = event.code;
     whichEventPlaceholder.textContent = whichCode;
     key = event.keyCode;
     codeEventPlaceholder.textContent = key;
+
+    // stores the history of the keypress into an array, then displays it
+    keyedHistory += eventKey;
+    keyedPlaceholder.textContent = keyedHistory;
 });
